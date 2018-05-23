c5f253b4dea89577117d90da558c84948c77fbe6
levisc8 <levisc8@gmail.com>
2018-05-23 16:10:52 +0200

    Remove Claudia's data

--- Master_R/Clean_Demography_Data.R
+++ Master_R/Clean_Demography_Data.R
@@ -86,7 +86,8 @@ save(CNClean, file = 'Carduus/CN_Clean.RData')
 rm(list = ls())
 DVRaw <- read.csv('Uncleaned_Data/Draba/Draba4R.csv',
                   stringsAsFactors = FALSE) %>%
-  filter(treatment != 'Herb')
+  filter(treatment == 'Control' |
+           treatment == 'Comp')
 
 DVClean <- DVRaw %>%
   setNames(c('Treatment', 'Plot', 'Plant', 'Survival', 'Fruits')) %T>%
